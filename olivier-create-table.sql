d719867b3d3dc836e4e36e2e7541ff52a65725e4
obareau <olivier.bareau@gmail.com>
2021-10-25 18:50:42 +0200

    Add files via upload
    Start-up commit

---
+++ olivier-create-table.sql
@@ -0,0 +1,11 @@
+-- create a table Student
+CREATE TABLE IF NOT EXISTS Student(
+    StudentId INTEGER PRIMARY KEY, -- not null value uniquely identify a row in this table
+    FirstName TEXT NOT NULL,
+    MiddleName TEXT,
+    Lastname TEXT NOT NULL,
+    Gpa REAL NOT NULL, --floating point number in SQL
+    Email TEXT NOT NULL UNIQUE
+);
+
+-- create a table for Professor EmployeeId, FirstName, lastName, Email)
